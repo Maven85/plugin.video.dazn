58fdf017ba5dd1333253b3cb0d9e7fbee17b8843
Maven85 <janis.semper@googlemail.com>
2024-02-02 19:43:47 +0100

    fixed playback of some streams

--- resources/lib/playback.py
+++ resources/lib/playback.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals
 
+from urllib.parse import quote_plus
 from .simple_requests.api import Request
 
 
@@ -40,10 +41,10 @@ class Playback:
     def parse_detail(self, details, cdn=''):
         for i in details:
             if cdn == self.clean_name([i['CdnName']])[0] or not cdn:
-                url = '{}{}{}={}'.format(i['ManifestUrl'], '&' if i['ManifestUrl'].find('?') > -1 else '?', i['CdnToken']['Name'], i['CdnToken']['Value'])
+                url = '{}{}{}={}'.format(i['ManifestUrl'], '&' if i['ManifestUrl'].find('?') > -1 else '?', i['CdnToken']['Name'], quote_plus(i['CdnToken']['Value']))
                 r = Request(self.plugin).head(url, headers={'user-agent': self.plugin.get_user_agent()})
                 if r.status_code == 200 and self.plugin.get_dict_value(r.headers, 'content-type').startswith('application/dash+xml'):
                     self.ManifestUrl = url
                     self.LaUrl = i['LaUrl']
-                    self.CdnToken = '{}={}'.format(i['CdnToken']['Name'], i['CdnToken']['Value'])
+                    self.CdnToken = '{}={}'.format(i['CdnToken']['Name'], quote_plus(i['CdnToken']['Value']))
                     break
